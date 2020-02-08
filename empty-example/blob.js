3b192f33e9370be46d42009689cededc0112562f
Diego G <dgpmakes@gmail.com>
2020-02-08 10:14:33 +0100

    Movement of Player

--- empty-example/blob.js
+++ empty-example/blob.js
@@ -2,6 +2,12 @@ function Blob(x, y, r){
   this.pos = createVector(x, y);
   this.r=r;
 
+  this.update = function(){
+    var vel = createVector(mouseX-width/2, mouseY-height/2);
+    vel.sub(this.pos);
+    vel.setMag(3);
+    this.pos.add(vel);
+  }
   this.show = function(){
     fill(255);
     ellipse(this.pos.x, this.pos.y , this.r*2, this.r*2);
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -4,16 +4,20 @@ var blobs = [];
 function setup() {
   createCanvas(600,600);
   blob = new Blob(width/2, height/2, 64);
-
-  for(var i = 0; i< 10; i++){
-    blobs[i]= new Blob(random(width), random(height), 16);
+  for(var i = 0; i< 50; i++){
+    var x = random(-width, width*2);
+    var y = random(-height, width*2);
+    blobs[i]= new Blob(x, y, 16);
   }
 }
 
 function draw() {
   background(0);
+
+  translate((width/2)-blob.pos.x, (width/2)-blob.pos.y);
   blob.show();
-  for(var i = 0; i< 10; i++){
+  blob.update();
+  for(var i = 0; i< 50; i++){
     blobs[i].show();
   }
 }
