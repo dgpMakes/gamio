998c22eceb59e61b77b89c8a695ca137e5fc865e
Raúl P <raporpe@gmail.com>
2020-02-13 11:30:38 +0100

    Formated  code

--- empty-example/blob.js
+++ empty-example/blob.js
@@ -1,40 +1,41 @@
-function Blob(x, y, r, z){
+function Blob(x, y, r, z) {
   this.pos = createVector(x, y);
-  this.r=r;
-  this.vel = createVector(0,0);
-  if(z==0){
-    fill(random(255),random(255),random(255));
+  this.r = r;
+  this.vel = createVector(0, 0);
+  if (z == 0) {
+    fill(random(255), random(255), random(255));
   }
-  if(z==1){
+  if (z == 1) {
     fill(255);
   }
-  this.update = function(){
-    var newvel = createVector(mouseX-width/2, mouseY-height/2);
+
+  this.update = function () {
+    var newvel = createVector(mouseX - width / 2, mouseY - height / 2);
     newvel.setMag(3);
     this.vel.lerp(newvel, 0.2);
     this.pos.add(this.vel);
   }
 
-  this.eats = function(other){
+  this.eats = function (other) {
     var d = p5.Vector.dist(this.pos, other.pos);
-    if (d < this.r + other.r){
-      var sum = PI * this.r * this.r + PI* other.r * other.r;
-      this.r = sqrt(sum/PI);
+    if (d < this.r + other.r) {
+      var sum = PI * this.r * this.r + PI * other.r * other.r;
+      this.r = sqrt(sum / PI);
       //this.r += other.r;
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
-  this.divide = function(){
+  this.divide = function () {
     var newR = this.r;
-    this.r = newR/2;
-    blob1 = new Blob (this.x+20, this.y+20, newR/2);
+    this.r = newR / 2;
+    blob1 = new Blob(this.x + 20, this.y + 20, newR / 2);
     blob1.show();
   }
 
-  this.show = function(){
-    ellipse(this.pos.x, this.pos.y , this.r*2, this.r*2);
+  this.show = function () {
+    ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
   }
 }
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,14 +1,14 @@
 var blob;
-var blobs =  [];
+var blobs = [];
 var zoom = 1;
 
 function setup() {
-  createCanvas(800,800);
+  createCanvas(800, 800);
   blob = new Blob(500, 500, 64, 1);
-  for(var i = 0; i< 600; i++){
+  for (var i = 0; i < 600; i++) {
     var x = random(-width, width);
     var y = random(-height, height);
-    blobs[i]= new Blob(x, y, 16, 0);
+    blobs[i] = new Blob(x, y, 16, 0);
 
   }
 }
@@ -16,7 +16,7 @@ function setup() {
 function draw() {
   background(0);
 
-  translate(width/2, height/2);
+  translate(width / 2, height / 2);
   var nez = blob / blob.r;
   var newzoom = 64 / blob.r;
   zoom = lerp(zoom, newzoom, 0.1);
@@ -24,18 +24,18 @@ function draw() {
   translate(-blob.pos.x, -blob.pos.y);
 
 
-  for(var i = blobs.length-1 ; i >= 0; i--){
+  for (var i = blobs.length - 1; i >= 0; i--) {
     blobs[i].show();
-    if(blob.eats(blobs[i])){
+    if (blob.eats(blobs[i])) {
       blobs.splice(i, 1);
     }
   }
 
   document.addEventListener('keyup', event => {
-  if (event.code === 'Space') {
-    blob.divide();
-  }
-})
+    if (event.code === 'Space') {
+      blob.divide();
+    }
+  })
 
   blob.show();
   blob.update();
