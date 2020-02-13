8c6b0a1daaf99a6452f9bb47f8f7d7c3d94b227f
Raúl P <raporpe@gmail.com>
2020-02-13 11:38:00 +0100

    Added some comments and changed from blob  to player

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,15 +1,16 @@
-var blob;
+var player;
 var blobs = [];
 var zoom = 1;
 
 function setup() {
   createCanvas(800, 800);
-  blob = new Blob(500, 500, 64, 1);
+  player = new Blob(500, 500, 64, 1);
+
+  // Set up small balls
   for (var i = 0; i < 600; i++) {
     var x = random(-width, width);
     var y = random(-height, height);
     blobs[i] = new Blob(x, y, 16, 0);
-
   }
 }
 
@@ -17,26 +18,25 @@ function draw() {
   background(0);
 
   translate(width / 2, height / 2);
-  var nez = blob / blob.r;
-  var newzoom = 64 / blob.r;
+  var newzoom = 64 / player.r;
   zoom = lerp(zoom, newzoom, 0.1);
   scale(zoom);
-  translate(-blob.pos.x, -blob.pos.y);
+  translate(-player.pos.x, -player.pos.y);
 
-
+  // Collision checker
   for (var i = blobs.length - 1; i >= 0; i--) {
     blobs[i].show();
-    if (blob.eats(blobs[i])) {
+    if (player.eats(blobs[i])) {
       blobs.splice(i, 1);
     }
   }
 
   document.addEventListener('keyup', event => {
     if (event.code === 'Space') {
-      blob.divide();
+      player.divide();
     }
   })
 
-  blob.show();
-  blob.update();
+  player.show();
+  player.update();
 }
